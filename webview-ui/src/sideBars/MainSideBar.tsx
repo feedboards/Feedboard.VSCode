2751e7c682fdd8073790391a0e1ba150280a9be7
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-26 13:24:58 +0300

    Update MainSideBar.tsx

--- webview-ui/src/sideBars/MainSideBar.tsx
+++ webview-ui/src/sideBars/MainSideBar.tsx
@@ -1,8 +1,15 @@
 import '../css/App.css';
 import { render } from '../utilities/render';
+import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
 
 const MainSideBar = () => {
-    return <div>MainSideBar</div>;
+    return (
+        <>
+            <main>
+                <VSCodeButton style={{ width: '100%' }}>Sign in via Azure</VSCodeButton>
+            </main>
+        </>
+    );
 };
 
 render(<MainSideBar />);
