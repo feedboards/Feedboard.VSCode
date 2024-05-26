61a333042bc1428c5dec53905ecac84a36213e21
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-26 13:17:58 +0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,15 @@ import { SideBarProvider } from './providers';
 import { MainPanel } from './panels';
 
 export function activate(context: vscode.ExtensionContext) {
-    const sideBarProvider = new SideBarProvider(context.extensionUri);
+    vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right).show();
 
-    const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+            'feedboard-sidebar-view',
+            new SideBarProvider(context.extensionUri)
+        )
+    );
 
-    item.show();
-
-    context.subscriptions.push(vscode.window.registerWebviewViewProvider('feedboard-sidebar-view', sideBarProvider));
     context.subscriptions.push(
         vscode.commands.registerCommand('feedboard.main-view', () => {
             MainPanel.render(context.extensionUri);
