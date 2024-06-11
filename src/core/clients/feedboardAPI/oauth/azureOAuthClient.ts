fda38f5efb3b2f41489e6ebde51718600dee8169
Katyara11 <maksymm.kacharaba@gmail.com>
2024-06-11 17:44:20 +0200

    Update azureOAuthClient.ts

--- src/core/clients/feedboardAPI/oauth/azureOAuthClient.ts
+++ src/core/clients/feedboardAPI/oauth/azureOAuthClient.ts
@@ -1,15 +1,15 @@
-import { APICore } from "../../apiCore";
+import { APICore } from '../../apiCore';
 
 const client = new APICore();
 
-export const getAzureLoginURI = () => {
+export const getAzureLoginURI = async () => {
     return client.get('AzureOAuth/login-url', null);
 };
 
-export const getAzureTokens = (code: string, state: string) => {
-    return client.get('AzureOAuth/process-code',  { code, state });
+export const getAzureTokens = async (code: string, state: string) => {
+    return await client.get('AzureOAuth/process-code', { code, state });
 };
 
-export const updateAzureAccessToken = (refreshToken: string) => {
-    return client.get('AzureOAuth/update-access-token', { refreshToken });
+export const updateAzureAccessToken = async (refreshToken: string) => {
+    return await client.get('AzureOAuth/update-access-token', { refreshToken });
 };
