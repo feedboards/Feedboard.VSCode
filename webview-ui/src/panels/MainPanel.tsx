477c9f7a683562ee0bdce6eb4bd26a8471827c7d
Katyara11 <maksymm.kacharaba@gmail.com>
2024-06-15 20:46:46 +0200

    add loading

--- webview-ui/src/panels/MainPanel.tsx
+++ webview-ui/src/panels/MainPanel.tsx
@@ -5,10 +5,11 @@ import {
     VSCodeDataGridRow,
     VSCodeDropdown,
     VSCodeOption,
+    VSCodeProgressRing,
 } from '@vscode/webview-ui-toolkit/react';
 import '../scss/mainPanel.scss';
 import { render, vscode } from '../utilities';
-import { useEffect, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import JsonView from '@uiw/react-json-view';
 import { vscodeTheme } from '@uiw/react-json-view/vscode';
 import { Subscription } from '@azure/arm-subscriptions';
@@ -17,23 +18,28 @@ import { ConsumerGroup, EHNamespace, Eventhub } from '@azure/arm-eventhub';
 import { EMainPanelCommands } from '../../../src/helpers';
 
 const MainPanel = () => {
-    // Dropdown States
     const [subscriptions, setSubscriptions] = useState<Subscription[]>();
+    const [subscriptionLoading, setSubscriptionLoading] = useState<boolean>(false);
     const [selectedSubscription, setSelectedSubscription] = useState<Subscription | undefined>(undefined);
 
     const [resourceGroups, setResourceGroups] = useState<ResourceGroup[]>();
+    const [resourceGroupLoading, setResourceGroupLoading] = useState<boolean>(false);
     const [selectedResourceGroup, setSelectedResourceGroup] = useState<ResourceGroup | undefined>(undefined);
 
     const [namespaces, setNamespaces] = useState<EHNamespace[]>();
+    const [namespaceLoading, setNamespaceLoading] = useState<boolean>(false);
     const [selectedNamespace, setSelectedNamespace] = useState<EHNamespace | undefined>(undefined);
 
     const [eventHubs, setEventHubs] = useState<Eventhub[]>();
+    const [eventHubLoading, setEventHubLoading] = useState<boolean>(false);
     const [selectedEventHub, setSelectedEventHub] = useState<ConsumerGroup | undefined>(undefined);
 
     const [consumerGroups, setConsumerGroups] = useState<Eventhub[]>();
+    const [consumerGroupLoading, setConsumerGroupLoading] = useState<boolean>(false);
     const [selectedConsumerGroup, setSelectedConsumerGroup] = useState<ConsumerGroup | undefined>(undefined);
 
     const [messages, setMessages] = useState<any[] | undefined>(undefined);
+    const [messageLoading, setMessageLoading] = useState<boolean>(false);
     const [selectedMessage, setSelectedMessages] = useState<any | undefined>(undefined);
 
     const [isLoggedInAzure, setIsLoggedInAzure] = useState<boolean>(false);
@@ -68,24 +74,27 @@ const MainPanel = () => {
 
                 case EMainPanelCommands.setSubscriptions:
                     setSubscriptions(payload);
+                    setSubscriptionLoading(false);
                     break;
 
                 case EMainPanelCommands.setResourceGroups:
                     setResourceGroups(payload);
+                    setResourceGroupLoading(false);
                     break;
 
                 case EMainPanelCommands.setNamespaces:
-                    console.log('setNamespaces', payload);
-
                     setNamespaces(payload);
+                    setNamespaceLoading(false);
                     break;
 
                 case EMainPanelCommands.setEventHubs:
                     setEventHubs(payload);
+                    setEventHubLoading(false);
                     break;
 
                 case EMainPanelCommands.setConsumerGroups:
                     setConsumerGroups(payload);
+                    setConsumerGroupLoading(false);
                     break;
 
                 case EMainPanelCommands.setIsLoggedInAzure:
@@ -95,6 +104,7 @@ const MainPanel = () => {
 
                     if (payload === true) {
                         vscode.postMessage({ command: EMainPanelCommands.getSubscriptions });
+                        setSubscriptionLoading(true);
                     }
                     break;
             }
@@ -139,6 +149,20 @@ const MainPanel = () => {
 
     const onClickSendMessage = () => {};
 
+    const addLoading = (state: boolean, Options: ReactElement) => {
+        return (
+            <>
+                {state && (
+                    <div className="main-panel__loading">
+                        <div>Loading...</div>
+                        <VSCodeProgressRing />
+                    </div>
+                )}
+                {!state && Options}
+            </>
+        );
+    };
+
     return (
         <main className="main-panel">
             <div className="main-panel__header">
@@ -164,17 +188,24 @@ const MainPanel = () => {
                                                 subscriptionId: x.subscriptionId,
                                             },
                                         });
+
+                                        setResourceGroupLoading(true);
                                     }
                                 },
                                 subscriptions
                             )
                         }>
-                        <VSCodeOption value="">Select a subscription</VSCodeOption>
-                        {subscriptions?.map((x: Subscription, index: number) => (
-                            <VSCodeOption key={index} value={x.subscriptionId}>
-                                {x.displayName}
-                            </VSCodeOption>
-                        ))}
+                        {addLoading(
+                            subscriptionLoading,
+                            <>
+                                <VSCodeOption value="">Select a subscription</VSCodeOption>
+                                {subscriptions?.map((x: Subscription, index: number) => (
+                                    <VSCodeOption key={index} value={x.subscriptionId}>
+                                        {x.displayName}
+                                    </VSCodeOption>
+                                ))}
+                            </>
+                        )}
                     </VSCodeDropdown>
                 </div>
                 {selectedSubscription !== undefined && (
@@ -203,17 +234,24 @@ const MainPanel = () => {
                                                     resourceGroupName: x.name,
                                                 },
                                             });
+
+                                            setNamespaceLoading(true);
                                         }
                                     },
                                     resourceGroups
                                 )
                             }>
-                            <VSCodeOption value="">Select a resource group</VSCodeOption>
-                            {resourceGroups?.map((x: ResourceGroup, index: number) => (
-                                <VSCodeOption key={index} value={x.name}>
-                                    {x.name}
-                                </VSCodeOption>
-                            ))}
+                            {addLoading(
+                                resourceGroupLoading,
+                                <>
+                                    <VSCodeOption value="">Select a resource group</VSCodeOption>
+                                    {resourceGroups?.map((x: ResourceGroup, index: number) => (
+                                        <VSCodeOption key={index} value={x.name}>
+                                            {x.name}
+                                        </VSCodeOption>
+                                    ))}
+                                </>
+                            )}
                         </VSCodeDropdown>
                     </div>
                 )}
@@ -240,17 +278,24 @@ const MainPanel = () => {
                                                     namespaceName: x.name,
                                                 },
                                             });
+
+                                            setEventHubLoading(true);
                                         }
                                     },
                                     namespaces
                                 )
                             }>
-                            <VSCodeOption value="">Select a namespace</VSCodeOption>
-                            {namespaces?.map((x: EHNamespace, index: number) => (
-                                <VSCodeOption key={index} value={x.name}>
-                                    {x.name}
-                                </VSCodeOption>
-                            ))}
+                            {addLoading(
+                                namespaceLoading,
+                                <>
+                                    <VSCodeOption value="">Select a namespace</VSCodeOption>
+                                    {namespaces?.map((x: EHNamespace, index: number) => (
+                                        <VSCodeOption key={index} value={x.name}>
+                                            {x.name}
+                                        </VSCodeOption>
+                                    ))}
+                                </>
+                            )}
                         </VSCodeDropdown>
                     </div>
                 )}
@@ -286,12 +331,17 @@ const MainPanel = () => {
                                         consumerGroups
                                     )
                                 }>
-                                <VSCodeOption value="">Select a consumer group</VSCodeOption>
-                                {consumerGroups?.map((x: ConsumerGroup, index: number) => (
-                                    <VSCodeOption key={index} value={x.name}>
-                                        {x.name}
-                                    </VSCodeOption>
-                                ))}
+                                {addLoading(
+                                    consumerGroupLoading,
+                                    <>
+                                        <VSCodeOption value="">Select a consumer group</VSCodeOption>
+                                        {consumerGroups?.map((x: ConsumerGroup, index: number) => (
+                                            <VSCodeOption key={index} value={x.name}>
+                                                {x.name}
+                                            </VSCodeOption>
+                                        ))}
+                                    </>
+                                )}
                             </VSCodeDropdown>
                         </div>
                     )}
@@ -324,27 +374,34 @@ const MainPanel = () => {
                                     </VSCodeDataGridCell>
                                 </VSCodeDataGridRow>
 
-                                {eventHubs?.map((x: Eventhub, index: number) => (
-                                    <VSCodeDataGridRow
-                                        key={index}
-                                        onClick={() => {
-                                            setSelectedEventHub(x);
+                                {addLoading(
+                                    eventHubLoading,
+                                    <>
+                                        {eventHubs?.map((x: Eventhub, index: number) => (
+                                            <VSCodeDataGridRow
+                                                key={index}
+                                                onClick={() => {
+                                                    setSelectedEventHub(x);
 
-                                            if (x !== undefined) {
-                                                vscode.postMessage({
-                                                    command: EMainPanelCommands.getConsumerGroups,
-                                                    payload: {
-                                                        subscriptionId: selectedSubscription.subscriptionId,
-                                                        resourceGroupName: selectedResourceGroup.name,
-                                                        namespaceName: selectedNamespace.name,
-                                                        eventHubName: x.name,
-                                                    },
-                                                });
-                                            }
-                                        }}>
-                                        <VSCodeDataGridCell gridColumn="1">{x.name}</VSCodeDataGridCell>
-                                    </VSCodeDataGridRow>
-                                ))}
+                                                    if (x !== undefined) {
+                                                        vscode.postMessage({
+                                                            command: EMainPanelCommands.getConsumerGroups,
+                                                            payload: {
+                                                                subscriptionId: selectedSubscription.subscriptionId,
+                                                                resourceGroupName: selectedResourceGroup.name,
+                                                                namespaceName: selectedNamespace.name,
+                                                                eventHubName: x.name,
+                                                            },
+                                                        });
+
+                                                        setConsumerGroupLoading(true);
+                                                    }
+                                                }}>
+                                                <VSCodeDataGridCell gridColumn="1">{x.name}</VSCodeDataGridCell>
+                                            </VSCodeDataGridRow>
+                                        ))}
+                                    </>
+                                )}
                             </VSCodeDataGrid>
                         )}
                 </div>
