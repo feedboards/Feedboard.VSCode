7d9bd5f7c31b922f1825c6105c0764eeba068c9f
Katyara11 <maksymm.kacharaba@gmail.com>
2024-08-20 20:38:15 +0200

    add github token into TokenHelper #7

--- src/helpers/TokenHelper.ts
+++ src/helpers/TokenHelper.ts
@@ -1,10 +1,11 @@
-import { AzureToken, TAzureTokenResponseDto } from '@feedboard/feedboard.core';
+import { AzureToken, GithubToken, TAzureTokenResponseDto, TGithubTokenResponseDto } from '@feedboard/feedboard.core';
 import { StoreHelper } from './storeHelper';
 import { EStoreKeywords } from '../types';
 
 export class TokenHelper {
     private static readonly _storeHelper: StoreHelper = StoreHelper.instance;
     private static readonly _azureToken: AzureToken = new AzureToken();
+    private static readonly _githubToken: GithubToken = new GithubToken();
 
     public async createAzureToken(token: TAzureTokenResponseDto): Promise<AzureToken> {
         await TokenHelper._storeHelper.storeValueAsync(EStoreKeywords.azureToken, JSON.stringify(token));
@@ -17,6 +18,22 @@ export class TokenHelper {
 
         if (json) {
             return new AzureToken(JSON.parse(json));
+        }
+
+        return null;
+    }
+
+    public async createGithubToken(token: TGithubTokenResponseDto): Promise<GithubToken> {
+        await TokenHelper._storeHelper.storeValueAsync(EStoreKeywords.githubToken, JSON.stringify(token));
+
+        return TokenHelper._githubToken.addTokenOrUpdate(token);
+    }
+
+    public async getGithubToken(): Promise<GithubToken | null> {
+        const json: string | undefined = await TokenHelper._storeHelper.getValueAsync(EStoreKeywords.githubToken);
+
+        if (json) {
+            return new GithubToken(JSON.parse(json));
         }
 
         return null;
